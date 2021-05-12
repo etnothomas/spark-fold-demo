0ac4cf28ee356391707b481a9dfc15be8cdea906
Thomas Jensen <thomas.jensen@gmail.com>
2021-05-12 19:32:30 +0200

    rec func

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -5,9 +5,9 @@ object Demo extends App {
 
   val spark = SparkSession.builder.appName("demo").master("local").getOrCreate()
 
-  val data = List(Row(99, 100, 101),
-    Row(119, 120, 121),
-    Row(149, 150, 151))
+  val data = List(Row(1, 100, 101),
+    Row(2, 120, 121),
+    Row(3, 150, 151))
 
   val schema = StructType(List(
     StructField("count0", IntegerType, nullable = true),
@@ -19,12 +19,23 @@ object Demo extends App {
 
   val df = spark.createDataFrame(rdd, schema)
 
-  val result = df.rdd.fold(Row(1, 0, 0)){
-    case (Row(prevcount0: Int, _, _), Row(_, count1: Int, count2: Int)) =>
-      Row(prevcount0 * count1  +  count2, count1, count2)
+  // does not really work as intended
+  val result1 = df.rdd.reduce {
+    case (Row(prevcount0: Int, _, _), Row(count0: Int, count1: Int, count2: Int)) =>
+      Row((prevcount0 * count1 + count2), count1, count2)
   }
 
-  val newDf = df.union(spark.createDataFrame(spark.sparkContext.parallelize(List(result)), schema))
-
+  val newDf = df.union(spark.createDataFrame(spark.sparkContext.parallelize(List(result1)), schema))
   newDf.take(10).foreach(println)
+
+  // collects to master and then transforms again
+  @scala.annotation.tailrec
+  def recFunc(dat: List[Row], prevResult: Int): Int = (dat, prevResult) match {
+    case (Nil, prevResult) => prevResult
+    case (rowList, previous) => recFunc(dat.tail, previous * rowList.head.getInt(1) + rowList.head.getInt(2))
+  }
+
+  val dat = df.collect().toList
+  val result2 = recFunc(dat, 0)
+  println(result2)
 }
